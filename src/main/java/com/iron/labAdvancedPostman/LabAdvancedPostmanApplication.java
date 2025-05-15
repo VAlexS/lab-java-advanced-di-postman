4b5ba57ae341eb3e4bfd63cc643e3cd7e3d3fc1e
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-15 15:07:30 +0200

    Commit inicial

---
+++ src/main/java/com/iron/labAdvancedPostman/LabAdvancedPostmanApplication.java
@@ -0,0 +1,13 @@
+package com.iron.labAdvancedPostman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAdvancedPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabAdvancedPostmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iron/labAdvancedPostman/LabAdvancedPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iron.labAdvancedPostman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAdvancedPostmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
